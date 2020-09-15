3df5567eb7a791e2ad4272644ea8375a431d256f
Omar Zain <omaren2010@gmail.com>
2020-09-15 04:39:54 +0300

    i fixed lab6 randomNum

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@
 var hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 var zainStore = {
 
